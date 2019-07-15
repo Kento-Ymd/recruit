3b9f045e2409c99361680cb6087f1d84c52221d5
Kento-Ymd <kento.yamada.miraidenshi@gmail.com>
2019-07-15 15:28:34 +0900

    完成間近

--- script.js
+++ script.js
@@ -1,66 +1,36 @@
-// ▼ カウントダウンタイマーの設定
-function CountdownTimer(elm, tl, mes) {
-    this.initialize.apply(this, arguments);
+var endDate = new Date('2020/04/01 00:00:00');
+var interval = 1000;
+
+
+function countdownTimer(){
+  var nowDate = new Date();
+  var period = endDate - nowDate ;
+  var addZero = function(n){return('0'+n).slice(-2);}
+  var addZeroDay = function(n){return('0'+n).slice(-3);}
+  if(period >= 0) {
+  var day = Math.floor(period / (1000 * 60 * 60 * 24));
+  period -=  (day　*(1000 * 60 * 60 * 24));
+  var hour = Math.floor(period / (1000 * 60 * 60));
+  period -= (hour *(1000 * 60 * 60));
+  var minutes =  Math.floor(period / (1000 * 60));
+  period -= (minutes * (1000 * 60));
+  var second = Math.floor(period / 1000);
+  var insert = "";
+  insert += '<span class="h">' + addZeroDay(day) +'日' + '</span>';
+  insert += '<span class="h">' + addZero(hour) + '時'+'</span>';
+  insert +=  '<span class="m">' + addZero(minutes) +'分' + '</span>';
+  insert += '<span class="s">' + addZero(second)+ '秒'+ '</span>';
+  document.getElementById('result').innerHTML = insert;
+  setTimeout(countdownTimer,10);
   }
-  CountdownTimer.prototype = {
-    initialize: function (elm, tl, mes) {
-      this.elem = document.getElementById(elm);
-      this.tl = tl;
-      this.mes = mes;
-    },
-    countDown: function () {
-      var timer = '';
-      var today = new Date();
-      var day = Math.floor((this.tl - today) / (24 * 60 * 60 * 1000));
-      var hour = Math.floor((day * 24) + ((this.tl - today) % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
-      var min = Math.floor(((this.tl - today) % (24 * 60 * 60 * 1000)) / (60 * 1000)) % 60;
-      var sec = Math.floor(((this.tl - today) % (24 * 60 * 60 * 1000)) / 1000) % 60 % 60;
-      var milli = Math.floor(((this.tl - today) % (24 * 60 * 60 * 1000)) / 10) % 100;
-      var me = this;
-  
-      if ((this.tl - today) > 0) {
-        if (hour) timer += '<span class="cdt_num">' + hour + '</span><small>時間</small>';
-        timer += '<span class="cdt_num">' + this.addZero(min) + '</span><small>分</small><span class="cdt_num">' + this.addZero(sec) + '</span><small>秒</small><span class="cdt_num">' + this.addZero(milli) + '</span>';
-        this.elem.innerHTML = timer;
-        tid = setTimeout(function () {
-          me.countDown();
-        }, 10);
-      } else {
-        this.elem.innerHTML = this.mes;
-        return;
-      }
-    },
-    addZero: function (num) {
-      return ('0' + num).slice(-2);
-    }
+  else{
+    var insert = "";
+    var number = 0;
+    insert += '<span class="h">' + number + number + '</span>';
+    insert +=  '<span class="m">' + number + number + '</span>';
+    insert += '<span class="s">' + number + number + '</span>';
+    document.getElementById('result').innerHTML = insert;
   }
-  
-  // ▼ 開始＆終了日時の指定と日付の判別
-  function CDT() {
-    var myD = Date.now(); // 1970/1/1午前0時から現在までのミリ秒
-    var start = new Date('2018-11-05T00:00+09:00'); // 開始日時の指定
-    var myS = start.getTime(); // 1970/1/1午前0時からの開始日時までのミリ秒
-    var end = new Date('2019-11-05T23:59+09:00'); // 終了日時の指定
-    var myE = end.getTime(); // 1970/1/1午前0時から終了日時までのミリ秒
-  
-    // 今日が開始日前か期間中か終了日後かの判別
-    if (myS <= myD && myE >= myD) {
-      var text = '<span>終了</span><span>まで</span>';
-      var tl = end;
-    } // 期間中
-    else if (myS > myD) {
-      var text = '<span>開催</span><span>まで</span>';
-      var tl = start;
-    } // 開始日前
-    else {
-      var text = "";
-    } // 終了日後
-  
-    var timer = new CountdownTimer('cdt_date', tl, '<small>終了しました</small>'); // 終了日後のテキスト
-    timer.countDown();
-    target = document.getElementById("cdt_txt");
-    target.innerHTML = text;
-  }
-  window.onload = function () {
-    CDT();
-  }
+}
+
+countdownTimer();
